d1163052feb514f1e03c01f4171024b34f6cdce1
Reddy Guuduru <rguuduru@salesforce.com>
2020-04-13 09:06:15 -0700

    moving config to upper part

--- utility.sh
+++ utility.sh
@@ -9,6 +9,9 @@ username=$3
 cd ..
 mkdir gitwork
 cd gitwork
+git config credential.helper 'cache --timeout=1200000'
+git config --global user.email "$email"
+git config --global user.name "$username"
 git clone git@github.com:wpsftest/forked-firstproject.git
 cd forked-firstproject/
 git remote -v
@@ -19,9 +22,6 @@ git merge upstream/master
 echo "about to do branch and checking out branch"
 git checkout -b new_branch
 echo "asetting configs"
-git config credential.helper 'cache --timeout=1200000'
-git config --global user.email "$email"
-git config --global user.name "$username"
 echo "beforepush things to forked branch"
 git remote -v
 git push origin new_branch --force > /dev/null
