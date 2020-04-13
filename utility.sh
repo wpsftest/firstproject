b041b261f9e300a3d1ea4880104770ea5b2ebe2c
Reddy Guuduru <rguuduru@salesforce.com>
2020-04-13 09:31:11 -0700

    moving things around

--- utility.sh
+++ utility.sh
@@ -18,6 +18,8 @@ git remote -v
 git remote add upstream git@github.com:rguuduru/firstproject.git
 git fetch upstream
 git checkout master
+git config --global user.email "$email"
+git config --global user.name "$username"
 git merge upstream/master
 echo "about to do branch and checking out branch"
 git checkout -b new_branch
